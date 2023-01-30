36631bd4d604de89b74f10e966a6fdcee26dd857
busi <116075924+busi0727@users.noreply.github.com>
2023-01-30 17:56:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from openpyxl import load_workbook, Workbook
 import time
 from apscheduler.schedulers.background import BackgroundScheduler
 
-token = 'os.environ['token']'
+token = 'token = open("token", "r").readline()'
 
 ############################게임 확률 명령어###############################
 
